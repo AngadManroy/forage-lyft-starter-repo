5425827b17b9de4cd306cfc7adc6648559dd6a0f
ANGAD MANROY <angadmanroy@gmail.com>
2023-10-16 18:04:38 +0530

    Changes for Task 2

--- car.py
+++ car.py
@@ -1,10 +1,12 @@
-from abc import ABC, abstractmethod
+from serviceable import Serviceable
 
 
-class Car(ABC):
-    def __init__(self, last_service_date):
-        self.last_service_date = last_service_date
+class Car(Serviceable):
+    def __init__(self, engine, battery):
+        #any new parts have to be written here
+        self.engine = engine
+        self.battery = battery
 
-    @abstractmethod
+    #No Longer an abstract method
     def needs_service(self):
-        pass
+        return self.engine.needds_service() or self.battery.needs_service()
